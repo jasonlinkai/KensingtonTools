92772a4aec896fd8a064be9d6fc8f50632201ea0
Jacky <jacky@iyes.dev>
2025-05-05 16:04:25 +0800

    feat: macos app sign and notarize

---
+++ electron-builder.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+/**
+ * @type {import('electron-builder').Configuration}
+ */
+module.exports = {
+  appId: process.env.APPLE_BUNDLE_ID,
+  productName: "Kensington Tools",
+  directories: {
+    output: "release"
+  },
+  files: [
+    "dist/**/*", 
+    "package.json",
+    "resources/**/*"
+  ],
+  asar: true,
+  icon: "resources/appIcon",
+  mac: {
+    category: "public.app-category.productivity",
+    target: [
+      {
+        target: "dmg",
+        arch: ["universal"]
+      }
+    ],
+    artifactName: "${productName}-universal.${ext}",
+    hardenedRuntime: true,
+    gatekeeperAssess: false,
+    entitlements: "mac/entitlements.mac.plist",
+    entitlementsInherit: "mac/entitlements.mac.plist",
+    identity: `${process.env.APPLE_TEAM_NAME} (${process.env.APPLE_TEAM_ID})`,
+    notarize: {
+      teamId: process.env.APPLE_TEAM_ID,
+    },
+  },
+  win: {
+    target: [
+      {
+        target: "msi",
+        arch: ["x64"]
+      }
+    ],
+    artifactName: "${productName}-setup-${version}.${ext}"
+  }
+}; 
